df3ee5c042d80f418f349cdb9bb67e5a04e58222
ramzy1996 <49083969+ramzy1996@users.noreply.github.com>
2021-08-25 18:21:10 +0530

    first commit

---
+++ model/users.js
@@ -0,0 +1,23 @@
+export default Users = [
+  {
+    id: 1,
+    email: "user1@gmail.com",
+    username: "user1",
+    password: "password1",
+    userToken: "token1",
+  },
+  {
+    id: 2,
+    email: "user2@gmail.com",
+    username: "user2",
+    password: "password2",
+    userToken: "token2",
+  },
+  {
+    id: 3,
+    email: "user@gmail.com",
+    username: "user",
+    password: "password",
+    userToken: "token",
+  },
+];
